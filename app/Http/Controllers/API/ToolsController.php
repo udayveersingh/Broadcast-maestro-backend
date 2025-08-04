2fe0d8ddc64e0c32b56bf613aa382971e3460e98
udayveersingh <uday.jbbn@gmail.com>
2025-08-04 13:48:11 +0530

    get tool id in adminusertool api

--- app/Http/Controllers/API/ToolsController.php
+++ app/Http/Controllers/API/ToolsController.php
@@ -75,7 +75,7 @@ class ToolsController extends Controller
             return response()->json(['success' => false, 'message' => "Invalid Request"], 401);
         } else {
             $user_tools =  AdminUserTool::where('user_id', '=', $userID)->select('name', 'budget', 'deadline', 'supplier')->latest()->get();
-            $admin_tools = Tool::select('name', 'budget', 'deadline', 'supplier')->latest()->get();
+            $admin_tools = Tool::select('tool_id','name', 'budget', 'deadline', 'supplier')->latest()->get();
             return response()->json(['success' => true, 'userTools' =>  $user_tools, 'adminTools' => $admin_tools], 200);
         }
     }
