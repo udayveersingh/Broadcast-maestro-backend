a6123bef94edac01fd633423a70abcf4f93aceb8
udayveersingh <uday.jbbn@gmail.com>
2025-08-22 16:17:02 +0530

    assigned  target_audience with tool ids

---
+++ database/migrations/2025_08_22_084303_create_campaign_templates_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaign_templates', function (Blueprint $table) {
+            $table->id();
+            $table->integer('campaign_id')->nullable();
+            $table->integer('tool_id')->nullable();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaign_templates');
+    }
+};
